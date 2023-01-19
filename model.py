e05db3757d85be8b58bacb5e7dccb2aa0207a65e
lkrist01 <kristilici@gmail.com>
2023-01-19 16:01:17 +0000

    write results in readme

--- model.py
+++ model.py
@@ -74,4 +74,4 @@ if __name__ == '__main__':
     df_test["anomaly"] = model.predict_outliers(df_test)
     print(df_test)
     
-    df.to_csv(r"result.csv", sep=' ', index=False, header=False)
+    df.to_csv(r"result.csv", sep=' ', index=False)
